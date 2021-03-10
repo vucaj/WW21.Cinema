a115a82c5dd91bd6d7631682a403b9a1eee981ab
Dusan Kostic <dusan.kole93@gmail.com>
2021-03-10 13:27:41 +0100

    feat/seat---repository-service-controller

--- WinterWorkShop.Cinema.API/Controllers/SeatsController.cs
+++ WinterWorkShop.Cinema.API/Controllers/SeatsController.cs
@@ -12,7 +12,7 @@ using WinterWorkShop.Cinema.Domain.Models;
 
 namespace WinterWorkShop.Cinema.API.Controllers
 {
-    [Authorize]
+    //[Authorize]
     [ApiController]
     [Route("api/[controller]")]
     public class SeatsController : ControllerBase
@@ -29,10 +29,11 @@ namespace WinterWorkShop.Cinema.API.Controllers
         /// </summary>
         /// <returns></returns>
         [HttpGet]
+        [Route("getAll")]
         public async Task<ActionResult<IEnumerable<SeatDomainModel>>> GetAsync()
         {
             IEnumerable<SeatDomainModel> seatDomainModels;
-            
+
             seatDomainModels = await _seatService.GetAllAsync();
 
             if (seatDomainModels == null)
@@ -41,6 +42,55 @@ namespace WinterWorkShop.Cinema.API.Controllers
             }
 
             return Ok(seatDomainModels);
+        }
+        [HttpGet]
+        [Route("getById")]
+        public async Task<ActionResult<SeatDomainModel>>GetByIdAsync([FromBody]SeatDomainModel domainModel)
+        {
+            var seatById = await _seatService.GetByIdAsync(new SeatDomainModel 
+            { 
+                Id = domainModel.Id
+            });
+
+            if (seatById == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(seatById);
+        }
+        [HttpGet]
+        [Route("getByAuditId")]
+        public async Task<ActionResult<IEnumerable<SeatDomainModel>>> GetAllByAuditoriumIdAsync([FromBody]AuditoriumDomainModel domainModel)
+        {
+            var seatByAuditId = await _seatService.GetAllByAuditoriumIdAsync(new AuditoriumDomainModel 
+            {
+                Id = domainModel.Id
+            });
+
+            if (seatByAuditId == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(seatByAuditId);
+        }
+
+        [HttpGet]
+        [Route("getBySeatType")]
+        public async Task<ActionResult<IEnumerable<SeatDomainModel>>> GetBySeatType([FromBody] SeatDomainModel domainModel )
+        {
+            var seatBySeatType = await _seatService.GetByAllSeatTypeAsync(new SeatDomainModel
+            {
+                SeatType = domainModel.SeatType
+            });
+
+            if(seatBySeatType == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(seatBySeatType);
         }
     }
 }
