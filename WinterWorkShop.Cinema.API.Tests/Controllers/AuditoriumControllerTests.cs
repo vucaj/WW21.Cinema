d572931f48e1ecbeaeed24f5960bc1eb31a3c219
vucaj <vucaj1998@outlook.com>
2021-03-12 15:12:13 +0100

    test: create auditorium successfully and with db exception

--- WinterWorkShop.Cinema.API.Tests/Controllers/AuditoriumControllerTests.cs
+++ WinterWorkShop.Cinema.API.Tests/Controllers/AuditoriumControllerTests.cs
@@ -3,9 +3,11 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using WinterWorkShop.Cinema.API.Controllers;
+using WinterWorkShop.Cinema.API.Models;
 using WinterWorkShop.Cinema.Domain.Interfaces;
 using WinterWorkShop.Cinema.Domain.Models;
 
@@ -77,6 +79,100 @@ namespace WinterWorkShop.Cinema.Tests.Controllers
             Assert.IsInstanceOfType(result, typeof(OkObjectResult));
             Assert.AreEqual(expectedStatusCode, ((OkObjectResult) result).StatusCode);
             
+        }
+
+        
+        [TestMethod]
+        public void PostAsync_Create_createAuditoriumResultModel_IsSuccessful_True_Auditorium()
+        {
+            //Arrange
+            int expectedStatusCode = 201;
+
+            CreateAuditoriumModel createAuditoriumModel = new CreateAuditoriumModel()
+            {
+                CinemaId = Guid.NewGuid(),
+                Name = "Sala1",
+                NumberOfSeats = 5,
+                SeatRows = 5
+            };
+
+            CreateAuditoriumResultModel createAuditoriumResultModel = new CreateAuditoriumResultModel()
+            {
+                IsSuccessful = true,
+                Auditorium = new AuditoriumDomainModel()
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Sala1",
+                    CinemaId = Guid.NewGuid()
+                },
+                ErrorMessage = null
+            };
+
+            Task<CreateAuditoriumResultModel> responseTask = Task.FromResult(createAuditoriumResultModel);
+
+            _auditoriumService = new Mock<IAuditoriumService>();
+            _auditoriumService.Setup(x => x.CreateAuditorium(It.IsAny<AuditoriumDomainModel>(), 5, 5)).Returns(responseTask);
+            AuditoriumsController auditoriumsController = new AuditoriumsController(_auditoriumService.Object);
+
+            //Act
+            var result = auditoriumsController.CreateAuditoriumAsync(createAuditoriumModel).ConfigureAwait(false)
+                .GetAwaiter().GetResult().Result;
+            var createdResult = ((CreatedResult) result).Value;
+            var auditoriumDomainModel = (AuditoriumDomainModel) createdResult;
+
+            //Assert
+            Assert.IsNotNull(auditoriumDomainModel);
+            Assert.IsInstanceOfType(result, typeof(CreatedResult));
+            Assert.AreEqual(expectedStatusCode, ((CreatedResult) result).StatusCode);
+        }
+
+        [TestMethod]
+        public void PostAsync_Create_Throw_DbException_Auditorium()
+        {
+            //Arrange
+            string expectedMessage = "Inner exception error message.";
+            int expectedStatusCode = 400;
+
+            CreateAuditoriumModel createAuditoriumModel = new CreateAuditoriumModel()
+            {
+                CinemaId = Guid.NewGuid(),
+                Name = "Sala1",
+                NumberOfSeats = 5,
+                SeatRows = 5
+            };
+
+            CreateAuditoriumResultModel createAuditoriumResultModel = new CreateAuditoriumResultModel()
+            {
+                Auditorium = new AuditoriumDomainModel()
+                {
+                    Id = Guid.NewGuid(),
+                    CinemaId = createAuditoriumModel.CinemaId,
+                    Name = "sala1"
+                },
+                IsSuccessful = true
+            };
+
+            Task<CreateAuditoriumResultModel> responseTask = Task.FromResult(createAuditoriumResultModel);
+            Exception exception = new Exception("Inner exception error message.");
+            DbUpdateException dbUpdateException = new DbUpdateException("Error", exception);
+
+            _auditoriumService = new Mock<IAuditoriumService>();
+            _auditoriumService.Setup(x => x.CreateAuditorium(It.IsAny<AuditoriumDomainModel>(), 5, 5))
+                .Throws(dbUpdateException);
+            AuditoriumsController auditoriumsController = new AuditoriumsController(_auditoriumService.Object);
+
+            //Act
+            var result = auditoriumsController.CreateAuditoriumAsync(createAuditoriumModel).ConfigureAwait(false)
+                .GetAwaiter().GetResult().Result;
+            var resultResponse = (BadRequestObjectResult) result;
+            var badObjectResult = ((BadRequestObjectResult) result).Value;
+            var errorResult = (ErrorResponseModel) badObjectResult;
+
+            //Assert
+            Assert.IsNotNull(resultResponse);
+            Assert.AreEqual(expectedMessage, errorResult.ErrorMessage);
+            Assert.IsInstanceOfType(result, typeof(BadRequestObjectResult));
+            Assert.AreEqual(expectedStatusCode, resultResponse.StatusCode);
         }
     }
 }
