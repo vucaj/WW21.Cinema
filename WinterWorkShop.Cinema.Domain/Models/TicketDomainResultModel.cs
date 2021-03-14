c4fe9aa7650bf1ed6c1e13daf0c1ef1c8273a60d
vucaj <vucaj1998@outlook.com>
2021-03-14 16:53:26 +0100

    feat: methods for iTicketService

---
+++ WinterWorkShop.Cinema.Domain/Models/TicketDomainResultModel.cs
@@ -0,0 +1,9 @@
+﻿namespace WinterWorkShop.Cinema.Domain.Models
+{
+    public class TicketDomainResultModel
+    {
+        public bool IsSuccessful { get; set; }
+        public string ErrorMessage { get; set; }
+        public TicketDomainModel Ticket { get; set; }
+    }
+}
